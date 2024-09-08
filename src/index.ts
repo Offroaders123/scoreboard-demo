1815723c6e70a2a0f2f6936a6ba728a63ff2898e
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-09-07 17:09:47 -0700

    Time Logging
    Seems to take about ~1 second+ in Node, ~2 seconds in Chrome, and... 6 seconds in Firefox? 🧐

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,10 @@ console.log(buffer);
 const data = new Uint8Array(buffer);
 console.log(data);
 
+console.time();
+console.log("Started the timer");
+
 const nbt = await read<any>(data);
 console.log(nbt.data.data);
+
+console.timeEnd();
