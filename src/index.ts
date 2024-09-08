c2c5921f8d8fd91a2e7b608139067159bfe895a4
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-09-07 17:45:09 -0700

    Player Stats JSON
    Showing a demo how you can use the safety of types in conjunction with the scripting of JS (lol) to easily get to other traversable formats like JSON.

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,17 @@ const nbt = await read<Scoreboard>(data);
 console.log(nbt.data.data);
 
 console.timeEnd();
+
+const scoreboardData = nbt.data.data;
+const { PlayerScores } = scoreboardData;
+
+const playerNames = new Set<string>();
+
+for (const score of PlayerScores) {
+  const { Name } = score;
+  const isUUID: boolean = Name.length === 36 && Name.match(/-/g)?.length === 4;
+  if (isUUID) continue;
+  playerNames.add(Name);
+}
+
+console.log(JSON.stringify([...playerNames], null, 2));
